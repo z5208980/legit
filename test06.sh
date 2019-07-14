8e382d98f5a0ab83cf15cf7d5160ed4fe25c4148
“z5208980” <z5208980@ad.unsw.edu.au>
2019-07-14 14:53:32 +1000

    test06.sh add

--- test06.sh
+++ test06.sh
@@ -1,21 +1,46 @@
 #!/bin/bash
 rm -rf .legit;
 
-#
+# TEST:
 #
 
 ./legit-init;
-echo a >a;
-echo b >b;
-echo c >c;
-echo d >d;
-echo e >e;
+for i in a b c d e f
+do
+    echo $i >$i;
+done
 ./legit-add a b c d e;
-./legit-rm --cached a;
-./legit-rm --cached b;
-./legit-rm --cached c;
-./legit-rm --cached d;
-./legit-rm --cached e;
-./legit-commit -m "commit-0";
 
-./legit-status;
+# Test undo the legit-add
+echo "TEST: legit-rm the entire index and commit";
+for i in a b c d e
+do
+    ./legit-rm --cached $i;
+done
+./legit-commit -m "commit-0";
+./legit-status | grep "^[abcdef] - ";
+
+# TEST legit-show to show there is nothing inside committed repo
+echo "TEST: legit-show the committed repo";
+for i in a b c d e
+do
+    ./legit-show 0:$i;
+done
+
+# TEST popping file one by one by legit-rm and commiting changes to latest deletion
+./legit-add a b c d e f;
+
+count=1;
+for j in a b c d e f
+do
+    echo "TEST: removing file $j from latest repo"
+    ./legit-rm --cached $j;
+    ./legit-commit -m "commit-$count $j deleted";
+    for i in a b c d e f
+    do
+        ./legit-show $count:$i;
+    done
+
+    count=`expr $count + 1`;
+done
+./legit-status | grep "^[abcdef] - ";
